d50ab762eb2c968d734f9426c1a45dfa9d91542a
oranheim <oranheim@gmail.com>
2019-09-29 21:44:06 +0200

    Public visibility on Cast

--- src/main/java/no/ssb/dc/api/el/Cast.java
+++ src/main/java/no/ssb/dc/api/el/Cast.java
@@ -1,20 +1,20 @@
 package no.ssb.dc.api.el;
 
-class Cast {
+public class Cast {
 
-    Float toFloat(String value) {
+    public Float toFloat(String value) {
         return Float.valueOf(value);
     }
 
-    Double toDouble(String value) {
+    public Double toDouble(String value) {
         return Double.valueOf(value);
     }
 
-    Integer toInteger(String value) {
+    public Integer toInteger(String value) {
         return Integer.valueOf(value);
     }
 
-    Long toLong(String value) {
+    public Long toLong(String value) {
         return Long.valueOf(value);
     }
 }
