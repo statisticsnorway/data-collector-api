3d6777e8e7c96cccb20b7b6d33400e18d9103d17
oranheim <oranheim@gmail.com>
2019-10-27 09:24:10 +0100

    More monitor metrics and dump threads to failed worker

--- src/main/java/no/ssb/dc/api/content/HealthContentStreamMonitor.java
+++ src/main/java/no/ssb/dc/api/content/HealthContentStreamMonitor.java
@@ -17,19 +17,24 @@ public class HealthContentStreamMonitor {
     final AtomicLong paginationDocumentCountRef = new AtomicLong(0);
     final AtomicLong paginationDocumentSizeRef = new AtomicLong(0);
     final AtomicLong lastPaginationDocumentWriteDurationRef = new AtomicLong(0);
+    final AtomicLong aggregatedPaginationDocumentWriteDurationRef = new AtomicLong(0);
 
     final AtomicLong entryBufferCountRef = new AtomicLong(0);
     final AtomicLong entryBufferSizeRef = new AtomicLong(0);
     final AtomicLong lastEntryBufferWriteDurationRef = new AtomicLong(0);
+    final AtomicLong aggregatedEntryBufferWriteDurationRef = new AtomicLong(0);
 
     final AtomicLong documentBufferCountRef = new AtomicLong(0);
     final AtomicLong documentBufferSizeRef = new AtomicLong(0);
     final AtomicLong lastDocumentBufferWriteDurationRef = new AtomicLong(0);
+    final AtomicLong aggregatedDocumentBufferWriteDurationRef = new AtomicLong(0);
 
     final AtomicLong publishedBufferCountRef = new AtomicLong(0);
     final AtomicLong lastPublishedBufferCountRef = new AtomicLong(0);
     final AtomicLong publishedPositionCountRef = new AtomicLong(0);
+    final AtomicLong publishedCountRef = new AtomicLong(0);
     final AtomicLong lastPublishWriteDurationRef = new AtomicLong(0);
+    final AtomicLong aggregatedPublishWriteDurationRef = new AtomicLong(0);
 
     public HealthContentStreamMonitor(Supplier<Boolean> contentStreamClosedCallback) {
         this.contentStreamClosedCallback = contentStreamClosedCallback;
@@ -53,6 +58,7 @@ public class HealthContentStreamMonitor {
 
     public void updateLastPaginationDocumentWriteDuration(long durationInMillis) {
         lastPaginationDocumentWriteDurationRef.set(durationInMillis);
+        aggregatedPaginationDocumentWriteDurationRef.addAndGet(durationInMillis);
     }
 
     public void incrementEntryBufferCount() {
@@ -65,6 +71,7 @@ public class HealthContentStreamMonitor {
 
     public void updateLastEntryBufferWriteDuration(long durationInMillis) {
         lastEntryBufferWriteDurationRef.set(durationInMillis);
+        aggregatedEntryBufferWriteDurationRef.addAndGet(durationInMillis);
     }
 
     public void incrementDocumentBufferCount() {
@@ -77,6 +84,7 @@ public class HealthContentStreamMonitor {
 
     public void updateLastDocumentBufferWriteDuration(long durationInMillis) {
         lastDocumentBufferWriteDurationRef.set(durationInMillis);
+        aggregatedDocumentBufferWriteDurationRef.addAndGet(durationInMillis);
     }
 
     public void addPublishedBufferCount(int bufferCount) {
@@ -92,7 +100,9 @@ public class HealthContentStreamMonitor {
     }
 
     public void updateLastPublishedPositionWriteDuration(long durationInMillis) {
+        publishedCountRef.incrementAndGet();
         lastPublishWriteDurationRef.set(durationInMillis);
+        aggregatedPublishWriteDurationRef.addAndGet(durationInMillis);
     }
 
     public ContentStreamInfo build() {
@@ -101,17 +111,20 @@ public class HealthContentStreamMonitor {
                 paginationDocumentCountRef.get(),
                 Math.round(HealthResourceUtils.divide(paginationDocumentSizeRef.get(), paginationDocumentCountRef.get())),
                 lastPaginationDocumentWriteDurationRef.get(),
+                Math.round(HealthResourceUtils.divide(aggregatedPaginationDocumentWriteDurationRef.get(), paginationDocumentCountRef.get())),
                 entryBufferCountRef.get(),
                 Math.round(HealthResourceUtils.divide(entryBufferSizeRef.get(), entryBufferCountRef.get())),
                 lastEntryBufferWriteDurationRef.get(),
+                Math.round(HealthResourceUtils.divide(aggregatedEntryBufferWriteDurationRef.get(), entryBufferCountRef.get())),
                 documentBufferCountRef.get(),
                 Math.round(HealthResourceUtils.divide(documentBufferSizeRef.get(), documentBufferCountRef.get())),
                 lastDocumentBufferWriteDurationRef.get(),
+                Math.round(HealthResourceUtils.divide(aggregatedDocumentBufferWriteDurationRef.get(), documentBufferCountRef.get())),
                 publishedBufferCountRef.get(),
                 lastPublishedBufferCountRef.get(),
                 publishedPositionCountRef.get(),
-                lastPublishWriteDurationRef.get()
-        );
+                lastPublishWriteDurationRef.get(),
+                Math.round(HealthResourceUtils.divide(aggregatedPublishWriteDurationRef.get(), publishedCountRef.get())));
     }
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -122,49 +135,61 @@ public class HealthContentStreamMonitor {
         @JsonProperty("page-document-count") public final Long paginationDocumentCount;
         @JsonProperty("avg-page-document-size-bytes") public final Integer averagePaginationDocumentSize;
         @JsonProperty("last-page-document-write-duration-millis") public final Long lastPaginationDocumentWriteDuration;
+        @JsonProperty("avg-page-document-write-duration-millis") public final Integer averagePaginationDocumentWriteDuration;
 
         @JsonProperty("page-entry-count") public final Long paginationEntryBufferCount;
         @JsonProperty("avg-page-entry-size-bytes") public final Integer averagePaginationEntryBufferSize;
         @JsonProperty("last-page-entry-write-duration-millis") public final Long lastEntryBufferWriteDuration;
+        @JsonProperty("avg-page-entry-write-duration-millis") public final Integer averageEntryBufferWriteDuration;
 
         @JsonProperty("entry-document-count") public final Long documentBufferCount;
         @JsonProperty("avg-entry-document-size-bytes") public final Integer averageDocumentBufferSize;
         @JsonProperty("last-entry-document-write-duration-millis") public final Long lastDocumentBufferWriteDuration;
+        @JsonProperty("avg-entry-document-write-duration-millis") public final Integer averageDocumentBufferWriteDuration;
 
         @JsonProperty("published-buffer-count") public final Long publishedBufferCount;
         @JsonProperty("last-published-buffer-count") public final Long lastPublishedBufferCount;
         @JsonProperty("published-position-count") public final Long publishedPositionCount;
         @JsonProperty("last-published-position-write-duration-millis") public final Long lastPublishWriteDuration;
+        @JsonProperty("avg-published-position-write-duration-millis") public final Integer averagePublishWriteDuration;
 
         ContentStreamInfo(String lastSeenTimestamp,
                           Long paginationDocumentCount,
                           Integer averagePaginationDocumentSize,
                           Long lastPaginationDocumentWriteDuration,
+                          Integer averagePaginationDocumentWriteDuration,
                           Long paginationEntryBufferCount,
                           Integer averagePaginationEntryBufferSize,
                           Long lastEntryBufferWriteDuration,
+                          Integer averageEntryBufferWriteDuration,
                           Long documentBufferCount,
                           Integer averageDocumentBufferSize,
                           Long lastDocumentBufferWriteDuration,
+                          Integer averageDocumentBufferWriteDuration,
                           Long publishedBufferCount,
                           Long lastPublishedBufferCount,
                           Long publishedPositionCount,
-                          Long lastPublishWriteDuration
+                          Long lastPublishWriteDuration,
+                          Integer averagePublishWriteDuration
         ) {
             this.lastSeenTimestamp = lastSeenTimestamp;
             this.paginationDocumentCount = paginationDocumentCount;
             this.averagePaginationDocumentSize = averagePaginationDocumentSize;
             this.lastPaginationDocumentWriteDuration = lastPaginationDocumentWriteDuration;
+            this.averagePaginationDocumentWriteDuration = averagePaginationDocumentWriteDuration;
             this.paginationEntryBufferCount = paginationEntryBufferCount;
             this.averagePaginationEntryBufferSize = averagePaginationEntryBufferSize;
             this.lastEntryBufferWriteDuration = lastEntryBufferWriteDuration;
+            this.averageEntryBufferWriteDuration = averageEntryBufferWriteDuration;
             this.documentBufferCount = documentBufferCount;
             this.averageDocumentBufferSize = averageDocumentBufferSize;
             this.lastDocumentBufferWriteDuration = lastDocumentBufferWriteDuration;
+            this.averageDocumentBufferWriteDuration = averageDocumentBufferWriteDuration;
             this.publishedBufferCount = publishedBufferCount;
             this.lastPublishedBufferCount = lastPublishedBufferCount;
             this.publishedPositionCount = publishedPositionCount;
             this.lastPublishWriteDuration = lastPublishWriteDuration;
+            this.averagePublishWriteDuration = averagePublishWriteDuration;
         }
     }
 }
