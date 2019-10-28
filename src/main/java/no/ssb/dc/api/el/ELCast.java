aaf1c0dddf833a3883739008e7d4bd5c0fc3c1e4
oranheim <oranheim@gmail.com>
2019-10-28 23:56:26 +0100

    Error handling for ELCast

--- src/main/java/no/ssb/dc/api/el/ELCast.java
+++ src/main/java/no/ssb/dc/api/el/ELCast.java
@@ -1,20 +1,46 @@
 package no.ssb.dc.api.el;
 
+import no.ssb.dc.api.util.CommonUtils;
+
 public class ELCast {
 
     public Float toFloat(String value) {
-        return Float.valueOf(value);
+        try {
+            return Float.valueOf(value);
+        } catch (RuntimeException | Error e) {
+            throw new EvaluationException("Error casting '" + value + "' to Float!\n" + CommonUtils.captureStackTrace(e));
+        } catch (Exception e) {
+            throw new EvaluationException("Error casting '" + value + "' to Float!\n" + CommonUtils.captureStackTrace(e));
+        }
     }
 
     public Double toDouble(String value) {
-        return Double.valueOf(value);
+        try {
+            return Double.valueOf(value);
+        } catch (RuntimeException | Error e) {
+            throw new EvaluationException("Error casting '" + value + "' to Double!\n" + CommonUtils.captureStackTrace(e));
+        } catch (Exception e) {
+            throw new EvaluationException("Error casting '" + value + "' to Double!\n" + CommonUtils.captureStackTrace(e));
+        }
     }
 
     public Integer toInteger(String value) {
-        return Integer.valueOf(value);
+        try {
+            return Integer.valueOf(value);
+        } catch (RuntimeException | Error e) {
+            throw new EvaluationException("Error casting '" + value + "' to Integer!\n" + CommonUtils.captureStackTrace(e));
+        } catch (Exception e) {
+            throw new EvaluationException("Error casting '" + value + "' to Integer!\n" + CommonUtils.captureStackTrace(e));
+        }
     }
 
     public Long toLong(String value) {
-        return Long.valueOf(value);
+        try {
+            return Long.valueOf(value);
+        } catch (RuntimeException | Error e) {
+            throw new EvaluationException("Error casting '" + value + "' to Long!\n" + CommonUtils.captureStackTrace(e));
+        } catch (Exception e) {
+            throw new EvaluationException("Error casting '" + value + "' to Long!\n" + CommonUtils.captureStackTrace(e));
+        }
     }
 }
