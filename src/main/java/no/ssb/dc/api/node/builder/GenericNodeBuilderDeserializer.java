49302cac4b6cd7f3eaba8dcbd55c3f39d49921b8
oranheim <oranheim@gmail.com>
2020-03-02 08:44:11 +0100

    Exploration testing

--- src/main/java/no/ssb/dc/api/node/builder/GenericNodeBuilderDeserializer.java
+++ src/main/java/no/ssb/dc/api/node/builder/GenericNodeBuilderDeserializer.java
@@ -7,17 +7,21 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 // https://github.com/statisticsnorway/json-stat.java/blob/master/src/main/java/no/ssb/jsonstat/v2/deser/DimensionDeserializer.java
-public class GenericNodeBuilderDeserializer extends StdDeserializer<GenericNodeBuilderDeserializer.Element.Builder> {
+public class GenericNodeBuilderDeserializer extends StdDeserializer<GenericNodeBuilderDeserializer.Node.Builder> {
 
     protected GenericNodeBuilderDeserializer() {
         super(AbstractBuilder.class);
     }
 
-    String indent(int depth) {
+    static String indent(int depth) {
         return Arrays.stream(new String[depth]).map(element -> " ").collect(Collectors.joining());
     }
 
@@ -26,8 +30,8 @@ public class GenericNodeBuilderDeserializer extends StdDeserializer<GenericNodeB
     }
 
     @Override
-    public Element.Builder deserialize(JsonParser parser, DeserializationContext context) throws IOException, JsonProcessingException {
-        Element.Builder elementBuilder = new Element.Builder(null);
+    public Node.Builder deserialize(JsonParser parser, DeserializationContext context) throws IOException, JsonProcessingException {
+        Node.Builder elementBuilder = new Node.Builder();
 
         StringBuilder builder = new StringBuilder();
 
@@ -38,7 +42,55 @@ public class GenericNodeBuilderDeserializer extends StdDeserializer<GenericNodeB
         return elementBuilder;
     }
 
-    void handleNode(int depth, JsonParser parser, DeserializationContext context, Element.Builder elementBuilder, StringBuilder builder) throws IOException {
+    void handleNode(int depth, JsonParser parser, DeserializationContext context, Node.Builder elementBuilder, StringBuilder builder) throws IOException {
+
+        JsonToken currentToken = parser.currentToken();
+        // check CurrentToken -> START_OBJECT
+        Node.Builder currentElementBuilder;
+
+        // new named property
+        if (currentToken == JsonToken.START_OBJECT && parser.currentName() != null) {
+            currentElementBuilder = new Node.Builder();
+            currentElementBuilder.name(parser.currentName());
+//            elementBuilder.addChild(currentElementBuilder);
+            // already have a named start-object (root is always unnamed)
+        } else {
+            currentElementBuilder = elementBuilder;
+        }
+
+        // check NextToken -> START_OBJECT
+        // check NextToken -> FIELD_NAME
+
+        JsonToken fieldToken = parser.nextToken();
+        if (fieldToken == JsonToken.START_OBJECT) {
+            // do nothing
+
+        } else if (fieldToken == JsonToken.FIELD_NAME) {
+            JsonToken nextValue;
+            while (PropertyType.isValueToken((nextValue = parser.nextValue()))) {
+                Property.Builder propertyBuilder = new Property.Builder();
+                propertyBuilder.name(parser.currentName());
+                propertyBuilder.value(parser.getCurrentValue(), PropertyType.of(nextValue));
+//                currentElementBuilder.addProperty(propertyBuilder);
+            }
+
+            if (nextValue == JsonToken.START_ARRAY) {
+                Array.Builder arrayBuilder = new Array.Builder();
+//                elementBuilder.addChild(arrayBuilder);
+            }
+
+        } else if (fieldToken == JsonToken.END_OBJECT || fieldToken == JsonToken.END_ARRAY) {
+
+
+        } else {
+            if (fieldToken != null) {
+                throw new IllegalStateException();
+            }
+        }
+
+    }
+
+    void handleNode_(int depth, JsonParser parser, DeserializationContext context, Node.Builder elementBuilder, StringBuilder builder) throws IOException {
 
         JsonToken currentToken = parser.currentToken();
 
@@ -52,29 +104,34 @@ public class GenericNodeBuilderDeserializer extends StdDeserializer<GenericNodeB
 
         } else if (fieldToken == JsonToken.FIELD_NAME) {
 
-            Property.Builder propertyBuilder = new Property.Builder();
-            propertyBuilder.name(parser.currentName());
-            propertyBuilder.value(parser.getValueAsString());
-
             builder(depth + 1, builder).append(String.format("%s %s ", fieldToken.name(), fieldToken.asString()))
                     .append(" => ").append(parser.currentName()).append(": ").append(parser.getValueAsString())
                     .append("\n");
 
-                JsonToken nextValue;
-                while ((nextValue = parser.nextValue()) == JsonToken.VALUE_STRING) {
-                    builder(depth, builder).append(" fieldValue: ").append(nextValue.name()).append(" -> ").append(parser.currentName()).append(": ").append(parser.getValueAsString()).append("\n");
-                }
+            JsonToken nextValue;
+            int n = 0;
+            while ((nextValue = parser.nextValue()) == JsonToken.VALUE_STRING) {
+                Property.Builder propertyBuilder = new Property.Builder();
+                propertyBuilder.name(parser.currentName());
+                propertyBuilder.value(parser.getValueAsString(), PropertyType.of(nextValue));
 
-                if (nextValue == JsonToken.START_ARRAY) {
-                    builder(depth, builder).append("START array: ").append(nextValue.name()).append(" ").append(nextValue.asString()).append(" ").append(currentToken.asString()).append(" ").append(parser.currentName()).append(" ").append(parser.getValueAsString()).append(" ");
-                    JsonToken nextToken = parser.nextToken();
-                    builder.append(nextToken).append(" ").append(nextToken.asString()).append(" ").append(parser.getValueAsString()).append("\n");
-                    handleNode(depth + 1, parser, context, elementBuilder, builder);
-                }
+                builder(depth, builder).append(" fieldValue: ").append(nextValue.name()).append(" -> ").append(parser.currentName()).append(": ").append(parser.getValueAsString()).append("\n");
+                n++;
+            }
+            builder.append(" <--- ").append(n).append("\n");
 
-                builder(depth, builder).append("END fieldValue: ").append(nextValue.name()).append(" -> ").append(parser.currentName()).append(": ").append(parser.getValueAsString()).append("\n");
-
+            if (nextValue == JsonToken.START_ARRAY) {
+                builder(depth, builder).append("START array: ").append(nextValue.name()).append(" ").append(nextValue.asString()).append(" ").append(currentToken.asString()).append(" ").append(parser.currentName()).append(" ").append(parser.getValueAsString()).append(" ");
+                JsonToken nextToken = parser.nextToken();
+                builder.append(nextToken).append(" ").append(nextToken.asString()).append(" ").append(parser.getValueAsString()).append("\n");
                 handleNode(depth + 1, parser, context, elementBuilder, builder);
+            }
+
+            builder(depth, builder).append("END fieldValue: ").append(nextValue.name()).append(" -> ").append(parser.currentName()).append(": ").append(parser.getValueAsString()).append("\n");
+
+            handleNode(depth + 1, parser, context, elementBuilder, builder);
+        } else {
+            //System.out.printf("-------------> %s%n", fieldToken);
         }
 
         if (fieldToken != null) {
@@ -83,46 +140,181 @@ public class GenericNodeBuilderDeserializer extends StdDeserializer<GenericNodeB
         }
     }
 
-    static class Element {
+    /*
+        Object, Array, Values
+     */
 
-        static class Builder {
-            private Element parent;
+    enum PropertyType {
+        STRING(JsonToken.VALUE_STRING),
+        INTEGER(JsonToken.VALUE_NUMBER_INT),
+        FLOAT(JsonToken.VALUE_NUMBER_FLOAT),
+        BOOLEAN(JsonToken.VALUE_TRUE, JsonToken.VALUE_FALSE);
 
-            public Builder(Element parent) {
-                this.parent = parent;
+        private final List<JsonToken> jsonTokenList;
+
+        PropertyType(JsonToken... jsonToken) {
+            jsonTokenList = List.of(jsonToken);
+        }
+
+        static PropertyType of(JsonToken jsonToken) {
+            for (PropertyType type : values()) {
+                if (type.jsonTokenList.contains(jsonToken)) {
+                    return type;
+                }
+            }
+            throw new IllegalArgumentException("PropertyType doesn't support JsonToken: " + jsonToken);
+        }
+
+        static boolean isValueToken(JsonToken jsonToken) {
+            for (PropertyType type : values()) {
+                if (type.jsonTokenList.contains(jsonToken)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        boolean isString() {
+            return this == STRING;
+        }
+
+        boolean isInteger() {
+            return this == INTEGER;
+        }
+
+        boolean isFloat() {
+            return this == FLOAT;
+        }
+
+        boolean isBoolean() {
+            return this == BOOLEAN;
+        }
+    }
+
+    static abstract class Base {
+        static abstract class Builder {
+        }
+    }
+
+    static class Array extends Base {
+
+        static class Builder extends Base.Builder {
+            private final List<Node> nodeList = new ArrayList<>();
+
+            public Builder() {
             }
 
-            Element build() {
-                return new Element();
+            Builder node(Node node) {
+                nodeList.add(node);
+                return this;
+            }
+
+            Array build() {
+                return new Array();
             }
         }
     }
 
-    public static class Property {
+    static class Node {
         public final String name;
-        public final String value;
+        public final Map<String, ? extends Base> children;
 
-        public Property(String name, String value) {
+        public Node(String name, Map<String, ? extends Base> children) {
             this.name = name;
-            this.value = value;
+            this.children = children;
+        }
+
+        @Override
+        public String toString() {
+            return "Element{" +
+                    "name='" + name + '\'' +
+                    ", children=" + children +
+                    '}';
         }
 
         static class Builder {
             private String name;
-            private String value;
+            private final Map<String, Base.Builder> children = new LinkedHashMap<>();
+
+            public Builder() {
+            }
 
             Builder name(String name) {
                 this.name = name;
                 return this;
             }
 
-            Builder value(String value) {
+            Builder addChild(Base.Builder childBuilder) {
+                children.put("", childBuilder);
+                return this;
+            }
+
+            Node build() {
+//                List<Node> nodeList = children.stream().map(Builder::build).collect(Collectors.toList());
+//                List<Property> propertyList = properties.values().stream().map(Property.Builder::build).collect(Collectors.toList());
+//                return new Node(name, nodeList, propertyList);
+                return null;
+            }
+        }
+    }
+
+    public static class Property extends Base {
+        public final String name;
+        public final Object value;
+        public final PropertyType type;
+
+        @Override
+        public String toString() {
+            return "Property{" +
+                    "name='" + name + '\'' +
+                    ", value=" + value +
+                    ", type=" + type +
+                    '}';
+        }
+
+        public Property(String name, Object value, PropertyType type) {
+            this.name = name;
+            this.value = value;
+            this.type = type;
+        }
+
+        String asString() {
+            return (String) value;
+        }
+
+        Integer asInteger() {
+            return (Integer) value;
+        }
+
+        Float asFloat() {
+            return (Float) value;
+        }
+
+        Boolean asBoolean() {
+            return (Boolean) value;
+        }
+
+        static class Builder extends Base.Builder {
+            private String name;
+            private Object value;
+            private PropertyType type;
+
+            public Builder() {
+            }
+
+            Builder name(String name) {
+                this.name = name;
+                return this;
+            }
+
+            Builder value(Object value, PropertyType type) {
                 this.value = value;
+                this.type = type;
                 return this;
             }
 
             Property build() {
-                return new Property(name, value);
+                return new Property(name, value, type);
             }
         }
     }
--- src/test/java/no/ssb/dc/api/node/builder/GenericNodeBuilderDeserializerTest.java
+++ src/test/java/no/ssb/dc/api/node/builder/GenericNodeBuilderDeserializerTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import no.ssb.dc.api.util.CommonUtils;
 import no.ssb.dc.api.util.JsonParser;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,11 +15,11 @@ class GenericNodeBuilderDeserializerTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(GenericNodeBuilderDeserializer.class);
 
-    static <R extends GenericNodeBuilderDeserializer.Element.Builder> R deserialize(String source, Class<R> builderClass) {
+    static <R extends GenericNodeBuilderDeserializer.Node.Builder> R deserialize(String source, Class<R> builderClass) {
         try {
             ObjectMapper mapper = JsonParser.createYamlParser().mapper();
             SimpleModule module = new SimpleModule();
-            module.addDeserializer(GenericNodeBuilderDeserializer.Element.Builder.class, new GenericNodeBuilderDeserializer());
+            module.addDeserializer(GenericNodeBuilderDeserializer.Node.Builder.class, new GenericNodeBuilderDeserializer());
             mapper.registerModule(module);
             return mapper.readValue(source, builderClass);
         } catch (IOException e) {
@@ -28,15 +27,64 @@ class GenericNodeBuilderDeserializerTest {
         }
     }
 
-    @Disabled
+//    @Disabled
     @Test
     void testGenericNodeBuilderDeserializer() {
         Path specPath = CommonUtils.currentPath().getParent().resolve("data-collection-consumer-specifications").resolve("specs");
         Path serializedSpec = specPath.resolve("toll-tvinn-test-spec.json");
         String jsonSpec = CommonUtils.readFileOrClasspathResource(serializedSpec.toString());
 
-        GenericNodeBuilderDeserializer.Element.Builder builder = deserialize(jsonSpec, GenericNodeBuilderDeserializer.Element.Builder.class);
+        GenericNodeBuilderDeserializer.Node.Builder builder = deserialize(jsonSpec, GenericNodeBuilderDeserializer.Node.Builder.class);
+        traverse(builder);
         LOG.trace("Builder: {}", builder);
         LOG.trace("Spec-path: {}\n{}", serializedSpec.toAbsolutePath(), jsonSpec);
     }
+
+    private void traverse(GenericNodeBuilderDeserializer.Node.Builder builder) {
+        GenericNodeBuilderDeserializer.Node rootNode = builder.build();
+        LOG.trace("{}", rootNode.name);
+    }
+
+    @Test
+    void testModel() {
+        GenericNodeBuilderDeserializer.Node.Builder rootBuilder = new GenericNodeBuilderDeserializer.Node.Builder();
+        rootBuilder.name("root");
+
+//        {
+//            GenericNodeBuilderDeserializer.Node.Builder childBuilder1 = new GenericNodeBuilderDeserializer.Node.Builder();
+//            childBuilder1.name("child1");
+//            childBuilder1.addProperty(new GenericNodeBuilderDeserializer.Property.Builder().name("prop1").value("value1", GenericNodeBuilderDeserializer.PropertyType.STRING));
+//            rootBuilder.addChild(childBuilder1);
+
+//            {
+//                GenericNodeBuilderDeserializer.Node.Builder childBuilder1_1 = new GenericNodeBuilderDeserializer.Node.Builder();
+//                childBuilder1_1.name("child1_1");
+//                childBuilder1_1.addProperty(new GenericNodeBuilderDeserializer.Property.Builder().name("prop1_1").value("value1_1", GenericNodeBuilderDeserializer.PropertyType.STRING));
+//                childBuilder1.addChild(childBuilder1_1);
+//            }
+//        }
+
+//        {
+//            GenericNodeBuilderDeserializer.Node.Builder childBuilder2 = new GenericNodeBuilderDeserializer.Node.Builder();
+//            childBuilder2.name("child2");
+//            childBuilder2.addProperty(new GenericNodeBuilderDeserializer.Property.Builder().name("prop2").value("value2", GenericNodeBuilderDeserializer.PropertyType.STRING));
+//            rootBuilder.addChild(childBuilder2);
+//        }
+
+//        StringBuilder stringBuilder = new StringBuilder();
+//        GenericNodeBuilderDeserializer.Node root = rootBuilder.build();
+//        traverse(0, root, stringBuilder);
+//        LOG.trace("\n{}", stringBuilder.toString());
+    }
+
+//    private void traverse(int depth, GenericNodeBuilderDeserializer.Node currentNode, StringBuilder stringBuilder) {
+//        String properties = currentNode.properties.stream().map(property -> property.value.toString()).collect(Collectors.joining(","));
+//        stringBuilder.append(String.format("%s(%s)%s%n%s", GenericNodeBuilderDeserializer.indent(depth), depth, currentNode.name,
+//                properties.isEmpty() ? "" : String.format("\t%s%s%n", GenericNodeBuilderDeserializer.indent(depth), properties)));
+//
+//        for (GenericNodeBuilderDeserializer.Node child : currentNode.children) {
+//            traverse(depth + 1, child, stringBuilder);
+//        }
+//    }
+
 }
