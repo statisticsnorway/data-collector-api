51657bbc522a200e6203b028b56a9249b931ae37
oranheim <oranheim@gmail.com>
2020-11-07 22:39:13 +0100

    Mered safe char-array to SecretManagerClient

--- src/main/java/no/ssb/dc/api/security/BusinessSSLResource.java
+++ src/main/java/no/ssb/dc/api/security/BusinessSSLResource.java
@@ -1,9 +1,5 @@
 package no.ssb.dc.api.security;
 
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Objects;
 
 public interface BusinessSSLResource extends AutoCloseable {
@@ -26,25 +22,5 @@ public interface BusinessSSLResource extends AutoCloseable {
     default boolean isPEM() {
         Objects.requireNonNull(getType());
         return "pem".equalsIgnoreCase(getType());
-    }
-
-    /**
-     * Convenience method that converts bytes to chars using UTF-8.
-     *
-     * Please notice: The input byte-array will be cleared after conversion.
-     *
-     * @param bytes input buffer
-     * @return char-array as utf8 or empty char-array
-     */
-    static char[] safeConvertBytesToCharArrayAsUTF8(final byte[] bytes) {
-        if (bytes == null) {
-            return new char[0];
-        }
-        CharBuffer cb = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(bytes));
-        final char[] chars = new char[cb.remaining()];
-        cb.get(chars);
-        Arrays.fill(bytes, (byte) 0);
-        cb.clear();
-        return chars;
     }
 }
